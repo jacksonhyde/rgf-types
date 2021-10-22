5d1cb6b12cf90ad4bcef58e22290a116ca41ffe2
Jackson Hyde <jacksonjhyde@gmail.com>
2021-10-22 22:09:31 +0100

    fixes

--- dist/RgfField.js
+++ dist/RgfField.js
@@ -1,12 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.State = void 0;
-var State = /** @class */ (function () {
-    function State() {
-        this.value = {};
-        this.formatted = '';
-        this.errors = [];
-    }
-    return State;
-}());
+class State {
+    value = {};
+    formatted = '';
+    errors = [];
+}
 exports.State = State;
