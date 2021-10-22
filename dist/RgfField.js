3896758b36fabff807abbf537a42efec7c37b138
Jackson Hyde <jacksonjhyde@gmail.com>
2021-10-22 23:28:32 +0100

    fix

--- dist/RgfField.js
+++ dist/RgfField.js
@@ -2,9 +2,11 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.State = void 0;
 class State {
-    value = {};
-    formatted = '';
-    errors = [];
+    constructor() {
+        this.value = {};
+        this.formatted = '';
+        this.errors = [];
+    }
 }
 exports.State = State;
 //# sourceMappingURL=RgfField.js.map
